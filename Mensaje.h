3ca67fd60bb28d68e4ba641edf9c3f17cc31f4ed
xnxxlein <joaquincastillaeguia7@gmail.com>
2023-06-25 21:50:59 -0500

    se incluyo e implemento el reclamo, pero tiene un bug con el getline, asi que se uso el cin en su lugar

--- Mensaje.h
+++ Mensaje.h
@@ -7,7 +7,19 @@ private:
     std::string reclamo;
 
 public:
-    Mensaje(const std::string& reclamo) : reclamo(reclamo) {}
-    void guardarReclamo();
+    Mensaje() = default;
+    void guardarReclamo(const std::string& nuevoReclamo) {
+        std::ofstream archivo("reclamo.txt", std::ios_base::app); // para que los nuevos reclamos puedan ser ingresados al final del archivo
+
+        if (archivo.is_open()) {
+            archivo << nuevoReclamo << "\n";
+            archivo.close();
+            std::cout << "Reclamo guardado correctamente." << std::endl;
+        }
+        else {
+            std::cout << "No se pudo abrir el archivo para guardar el reclamo." << std::endl;
+        }
+    }
+
     
 };
